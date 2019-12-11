253db1df9360bae2a4825ed24838bb9a4f579697
Crystal Soo Lee <soo.leesj@gmail.com>
2019-12-11 11:00:22 -0800

    location query is working

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const PORT = process.env.PORT || 3000;
 const express = require('express');
 const cors = require('cors');
-// const superagent = require('superagent');
+const superagent = require('superagent');
 
 const app = express();
 require('dotenv').config();
@@ -15,51 +15,25 @@ app.get('/', (request,response) => {
 });
 
 app.get('/location', (request,response) => {
-  try {
-    const locationData = getLatLong(request.query.data);
-    response.send(locationData);
-  }
-  catch(error) {
-    console.error(error);
-    response.status(500).send('Status 500: Internal Server Error');
-  }
+  const locationName = request.query.data;
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${locationName}&key=${process.env.GEOCODE_API_KEY}`;
+  superagent.get(url).then(data => {
+    const geoData = data.body;
+    const city = request.query.data;
+    const locationData = new Location(city, geoData);
+    response.status(200).send(locationData);
+  }).catch(err => {
+    console.error(err);
+    response.status(500).send('Status 500: Internal Server Error')
+  });
 });
 
-app.get('/weather', (request,response) => {
-  try {
-    const weatherQuery = getWeather();
-    console.log('weatherQuery :', weatherQuery);
-    response.send(weatherQuery);
-  }
-  catch(error) {
-    console.error(error);
-    response.status(500).send('Status 500: Internal Server Error');
-  }
-});
 
-function getLatLong(location) {
-  const geoData = require('./data/geo.json');
-  const locationData = {
-    search_query: location,
-    formatted_query: geoData.results[0].formatted_address,
-    latitude: geoData.results[0].geometry.location.lat,
-    longitude: geoData.results[0].geometry.location.lng,
-  }
-  console.log('locationData :', locationData);
-  return locationData;
-}
-
-function getWeather() {
-  const weatherForcast = require('./data/darksky.json');
-  let weatherDetails = [];
-  weatherForcast.daily.data.forEach(weatherObj => weatherDetails.push(new Weather(weatherObj)));
-
-  return weatherDetails;
-}
-
-function Weather(weatherObj) {
-  this.forecast = weatherObj.summary,
-  this.time = new Date(weatherObj.time * 1000).toDateString();
+function Location(city, geoData){
+  this.search_query = city;
+  this.formatted_query = geoData.results[0].formatted_address;
+  this.latitude = geoData.results[0].geometry.location.lat;
+  this.longitude = geoData.results[0].geometry.location.lng;
 }
 
 app.listen(PORT, () => {
